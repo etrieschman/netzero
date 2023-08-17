bf71656cc95d87dbea9bbb4491addc07e4e44d2f
Erich Trieschman <erich.j.trieschman@gmail.com>
2023-08-17 16:19:55 -0400

    initialize repo

---
+++ code/bulk_data_download.py
@@ -0,0 +1,98 @@
+# Code leveraged from: https://github.com/USEPA/cam-api-examples/blob/main/Python/bulk_data_api_demo.py
+
+# %%
+import requests
+import json
+import sys
+from datetime import datetime
+from datetime import date
+from tqdm import tqdm
+import os
+
+# The bulk data api allows you to download prepackaged data sets. There are two endpoints for obtaining bulk data.
+# The first is the /bulk-files endpoint which returns metadata about files. This metadata includes the path to the
+# file.  The second is the /easey/bulk-files endpoint which along with the path, returns the actual file.
+
+# Set API key
+API_KEY = 'z6klBZ8QocrDVSam0LpGFfc5CQCgFaoawmH0eUjn'
+# S3 bucket url base + s3Path (in get request) = the full path to the files
+URL_BASE = 'https://api.epa.gov/easey/bulk-files/'
+URL_METADATA = "https://api.epa.gov/easey/camd-services/bulk-files"
+PATH_DATA = '../data/'
+parameters = {'api_key': API_KEY}
+
+# %%
+# GET BULK FILES
+def get_bulk_files(parameters):
+    # executing get request
+    response = requests.get(URL_METADATA, params=parameters)
+
+    # printing the response error message if the response is not successful
+    print("Status code: "+str(response.status_code))
+    if (int(response.status_code) > 399):
+        sys.exit("Error message: "+response.json()['error']['message'])
+
+    # converting the content from json format to a data frame
+    resjson = response.content.decode('utf8').replace("'", '"')
+    bulkFiles = json.loads(resjson)
+    return bulkFiles
+
+# get files
+bulkFiles = get_bulk_files(parameters)
+# print out unique data types in the bulk data files
+print('Unique data types in the bulk data files:')
+print(set([fileObj['metadata']['dataType'] for fileObj in bulkFiles]))
+
+# %%
+# DOWNLOAD DATA
+def download_files(filesToDownload):
+    if len(filesToDownload) > 0:
+        # loop through all files and download them
+        for fileObj in tqdm(filesToDownload):
+            url = URL_BASE+fileObj['s3Path']
+            print('Full path to file on S3: '+url)
+            # download and save file
+            response = requests.get(url, params=parameters)
+            # save file to disk in the data folder
+            with open(PATH_DATA+fileObj['filename'], 'wb') as f:
+                f.write(response.content)
+    else:
+        print('No files to download')
+
+
+# %%
+# DOWNLOAD FACILITY DATA
+# facility data
+facFiles = [fileObj for fileObj in bulkFiles if 
+            (fileObj['metadata']['dataType']=='Facility')]
+facFilters = {'minYear': 2018}
+
+filesToDownload = [fileObj for fileObj in facFiles if 
+                   (int(fileObj['metadata']['year']) >= facFilters['minYear'])]
+print('Number of files to download: '+str(len(filesToDownload)))
+download_files(filesToDownload)
+
+
+
+# %%
+# DOWNLOAD EMISSIONS DATA
+# facility data
+emFiles = [fileObj for fileObj in bulkFiles if 
+            (fileObj['metadata']['dataType']=='Emissions')]
+print('Emission granularity:', 
+      set(fileObj['metadata']['dataSubType'] for fileObj in emFiles))
+emFiles = [fileObj for fileObj in emFiles if
+           (fileObj['metadata']['dataSubType']=='Daily')]
+emFilters = {'minYear': 2020}
+
+filesToDownload = [fileObj for fileObj in emFiles if 
+                   (int(fileObj['metadata']['year']) >= emFilters['minYear'])]
+print('Number of files to download: '+str(len(filesToDownload)))
+download_files(filesToDownload)
+
+
+
+
+
+
+# %%
---
+++ code/process_epa_data.py
@@ -0,0 +1,61 @@
+# %%
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+import plotly.figure_factory as ff
+from shapely.geometry import Point
+import matplotlib.pyplot as plt
+import os
+from tqdm import tqdm
+
+PATH_DATA = '../data/'
+
+pd.set_option('display.max_columns', None)
+
+def readin_data(key, coll_cols=None):
+    files = [f for f in os.listdir(PATH_DATA) if key in f.lower()]
+    df = pd.DataFrame({})
+    for f in tqdm(files):
+        df_in = pd.read_csv(PATH_DATA + f, low_memory=False)
+        if coll_cols is not None:
+            df_in['quarter'] = pd.to_datetime(df_in.Date).dt.to_period('Q')
+            df_in = df_in.groupby(coll_cols['id']+['quarter'])[coll_cols['val']].sum().reset_index()
+        df = pd.concat([df_in, df], ignore_index=True, axis=0)
+    return df
+
+# %%
+# READ IN data
+facdf = readin_data('facility')
+# emdf = readin_data('emissions')
+
+# %%
+# READ IN EMISSIONS
+col_em = {'id': ['State', 'Facility Name', 'Facility ID', 'Unit ID'],
+          'val': ['Gross Load (MWh)', 'Steam Load (1000 lb)', 
+                  'SO2 Mass (short tons)', 'CO2 Mass (short tons)', 
+                  'NOx Mass (short tons)', 'Heat Input (mmBtu)']}
+emdf = readin_data('emissions', col_em)
+
+
+# %%
+# VISUALIZE EMISSIONS DATA
+facility_units = emdf[['Facility ID', 'Unit ID']].drop_duplicates().values
+for f, u in facility_units[:10]:
+    em = emdf.loc[(emdf['Facility ID'] == f) & (emdf['Unit ID'] == u), ].sort_values('quarter')
+    plt.plot(em.quarter.dt.to_timestamp().values, em['Gross Load (MWh)'].values)
+plt.title('Sample unit-level emissions data by quarter')
+
+# %%
+# VISUALIZE FACILITY DATA
+temp = (facdf
+        .loc[~facdf.State.isin(['AK', 'HI', 'PR'])]
+        .groupby(['Latitude', 'Longitude', 'Year']).agg({'Facility ID':'count'})
+        .reset_index())
+geometry = [Point(xy) for xy in zip(temp['Longitude'], temp['Latitude'])]
+geotemp = gpd.GeoDataFrame(temp, geometry=geometry)
+for y in geotemp.Year.drop_duplicates().values:
+    geotempyear = geotemp.loc[geotemp.Year == y]
+    geotempyear.plot(column='Facility ID', alpha=0.5, legend=True)
+    plt.title(f'Count of facilities in Year {y}')
+    plt.show()
+# %%
