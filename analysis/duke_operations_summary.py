93c64629314423b3ff2e4fa150446a9ce6a9197a
Erich Trieschman <erich.j.trieschman@gmail.com>
2023-11-13 17:20:48 -0500

    duke analysis; start entity alignment

--- analysis/duke_operations_summary.py
+++ analysis/duke_operations_summary.py
@@ -1,15 +1,18 @@
 # %%
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+import geopandas as gpd
 
 # global variables
 PATH_DATA = '../data/'
 PATH_PROCESSED = PATH_DATA + 'processed/'
+PATH_RESULTS = '../results/'
 DENOM, ROUND = 1e6, 2
 
 # options
 pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', 150)
+pd.set_option('display.max_rows', 100)
 
 # read in data
 gdf = pd.read_parquet(PATH_PROCESSED + 'eia860_generator.parquet')
@@ -43,9 +46,10 @@ dgg = pd.merge(left=dg, right=gendf.drop(columns=['energy_source_1', 'status']),
 assert (0 == dgg[['year', 'utility_id', 'plant_code', 'generator_id']].duplicated().sum())
 dgge = pd.merge(left=dgg, right=emdf.drop(columns='gid'), how='left', on=['year', 'plant_code', 'generator_id'])
 assert (0 == dgge[['year', 'utility_id', 'plant_code', 'generator_id']].duplicated().sum())
-dgge = pd.merge(left=dgge, right=pdf[['year', 'plant_code', 'state_plant']], how='left', 
+dgge = pd.merge(left=dgge, right=pdf[['year', 'plant_code', 'state_plant', 'latitude', 'longitude']], how='left', 
                 on=['year', 'plant_code'])
 assert (0 == dgge[['year', 'utility_id', 'plant_code', 'generator_id']].duplicated().sum())
+
 
 # %%
 # CREATE FLAGS
@@ -54,14 +58,22 @@ dgge['is_duke'] = dgge.utility_id.isin(duke_utilities.utility_id.values)
 dgge['is_duke_startyear'] = (dgge.year == start_year) & dgge.utility_id.isin(duke_utilities.utility_id.values)
 dgge['is_duke_startyear'] = dgge.groupby(['plant_code', 'generator_id'])['is_duke_startyear'].transform('max')
 dgge['capacity_factor'] = dgge.net_gen_tot_an / (dgge.nameplate_capacity_mw*8760)
-
+# subset to generators that are either operating or have emissoins/generation data
+dgge['is_operating_status'] = dgge.status.isin(['OP', 'OA', 'OS', 'SB'])
+dgge['is_operating'] = (
+    (dgge.co2_mass_short_tons_gen_923 > 0)
+    | (dgge.co2_mass_short_tons_gen > 0)
+    | (dgge.net_gen_tot_an > 0)
+)
+dgge_op = dgge.loc[dgge.is_operating | dgge.is_operating_status]
 
 
 # %%
+# TOPLINE OPERATIONS TABLE
 groupers = [['state_plant', 'year'], ['energy_source_1', 'year'], ['year']]
 summ_df = pd.DataFrame()
 for group in groupers:
-    df = (dgge.groupby(group)
+    df = (dgge_op.groupby(group)
         .agg({'utility_id':'nunique', 'plant_code':'nunique', 'gid':'nunique',
             'nameplate_capacity_mw':'sum', 'co2_mass_short_tons_gen':'sum',
             'co2_mass_short_tons_gen_923':'sum',
@@ -70,9 +82,99 @@ for group in groupers:
             .reset_index())
     summ_df = pd.concat([df, summ_df], ignore_index=True)
 
-# %%
 vars_id = [('year',''), ('energy_source_1',''), ('state_plant','')]
 vars_other = [col for col in summ_df.columns if col not in vars_id]
 summ_df[vars_id] = summ_df[vars_id].fillna('TOTAL')
-summ_df[vars_id + vars_other]
+summ_df[vars_id + vars_other].to_csv(PATH_RESULTS + 'duke/summary.csv', index=False)
+
 # %%
+# GENERATION MIX BY STATE
+import seaborn as sns
+gm = (dgge_op
+      .groupby(['year', 'state_plant', 'energy_source_1'])
+      .agg({'net_gen_tot_an':'sum'}) / 1e6).reset_index()
+gm['net_gen_tot_an_state'] = gm.groupby(['year', 'state_plant'])['net_gen_tot_an'].transform('sum')
+gm['net_gen_pct'] = gm.net_gen_tot_an / gm.net_gen_tot_an_state
+
+# Pivot the data to get energy type breakdown for each state by year
+gmt = gm.pivot_table(
+    values='net_gen_pct', index=['state_plant', 'year'], 
+    columns='energy_source_1', aggfunc='sum')
+# Fill missing values with 0 (if any)
+gmt = gmt.fillna(0).reset_index()
+# Now we plot a stacked bar chart for each state
+states = gm['state_plant'].unique()
+states = ['NC', 'SC', 'FL', 'IN', 'OH', 'KY']
+
+# Set up the matplotlib figure and axes
+figscale=5
+ncols=2
+fig, axes = plt.subplots(
+    nrows=len(states)//ncols, ncols=ncols, figsize=(5*ncols, 4*len(states)//ncols), sharex=True, sharey=True)
+# Loop through each state and create a stacked bar chart
+for ax, state in zip(axes.flatten(), states):
+    state_data = gmt.loc[gmt.state_plant == state]
+    state_data.plot(kind='bar', x='year', legend=False, stacked=True, cmap='tab20', ax=ax, title=f'Net Generation in {state}')
+    ax.set_xlabel('Year')
+    ax.set_ylabel('Net Generation (pct)')
+axes[2,1].legend(title='Energy Type')
+
+# Adjust the layout to prevent overlap
+plt.tight_layout()
+plt.show()
+
+
+
+# %%
+# MAP OF OPERATIONS
+year = 2018
+
+# Read in the US states shapefile using GeoPandas
+gdf_states = gpd.read_file(PATH_DATA + 'resources/cb_state_boundaries/cb_2018_us_state_20m.shp')
+gdf_states = gdf_states.loc[~gdf_states.STUSPS.isin(['HI', 'AK', 'PR'])]
+
+# MAP GENERATION
+# Convert the DataFrame to a GeoDataFrame
+dgge_map = (dgge_op
+            .loc[(dgge_op.year == year)]
+            .groupby(['latitude', 'longitude'])
+            [['net_gen_tot_an']].sum()).reset_index()
+gdf = gpd.GeoDataFrame(dgge_map, geometry=gpd.points_from_xy(dgge_map.longitude, dgge_map.latitude))
+fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+gdf_states.geometry.plot(ax=ax, linewidth=0.8, color='lightgrey', alpha=0.5, edgecolor='black')
+gdf.plot(ax=ax, 
+         column='net_gen_tot_an', cmap='YlOrRd', markersize=np.sqrt(gdf.net_gen_tot_an)/10,
+         legend=True, legend_kwds={'label': "Electricity Generation (MWh)",
+                                   'shrink':0.75},
+         alpha=0.75)
+plt.xlim((-107, -75))
+plt.title(f'All Duke Energy plants in {year}, by total annual generation')
+plt.show()
+
+# MAP FUEL TYPES
+dgge_map = (dgge_op
+            .loc[(dgge_op.year == year) & (dgge_op.net_gen_tot_an > 0),
+                 ['latitude', 'longitude', 'energy_source_1']]
+             .drop_duplicates().reset_index(drop=True))
+gdf = gpd.GeoDataFrame(dgge_map, geometry=gpd.points_from_xy(dgge_map.longitude, dgge_map.latitude))
+fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+gdf_states.geometry.plot(ax=ax, linewidth=0.8, color='lightgrey', edgecolor='black')
+gdf.plot(ax=ax, 
+         column='energy_source_1', s=250, cmap='tab10', 
+         legend=True,
+         alpha=0.75)
+plt.xlim((-107, -75))
+plt.title(f'Any Duke Energy plants producing electricity in {year}, by energy source')
+plt.show()
+
+# %%
+# RETIREMENTS AND NEW PLANTS
+mask_gen_or_em = ((dgge.net_gen_tot_an.notna() & (dgge.net_gen_tot_an != 0))
+                   | (dgge.co2_mass_short_tons_gen > 0))
+dgge['retired_ops'] = (dgge.year >= dgge.dt_operation_end.dt.year)
+dgge['retired_status'] = (dgge.status == 'RE')
+dgge.loc[(dgge.retired_ops | dgge.retired_status) & mask_gen_or_em]
+
+
+# %%
+# DIVESTMENTS
--- code/align/entities.py
+++ code/align/entities.py
@@ -1,6 +1,7 @@
 # %%
 # SETUP
 import pandas as pd
+import numpy as np
 
 # global variables
 PATH_PROCESSED = '../data/processed/'
@@ -11,10 +12,88 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', 150)
 
 # %%
-# TODO: Align EIA entity properties (ones that persist over time)
+# LOAD DATASETS
+def load_datasets(path=PATH_PROCESSED):
+    gdf = pd.read_parquet(path + 'eia860_generator.parquet')
+    pdf = pd.read_parquet(path + 'eia860_plant.parquet')
+    udf = pd.read_parquet(path + 'eia860_utility.parquet')
+    odf = pd.read_parquet(path + 'eia860_ownership.parquet')
+
+    # unique ids
+    gdf['gid'] = gdf.plant_code.astype(str) + '_' + gdf.generator_id
+    odf['oid'] = odf.ownership_id.astype(str) + '_' + odf.plant_code.astype(str) + '_' + odf.generator_id
+
+    return gdf, pdf, udf, odf
+gdf, pdf, udf, odf = load_datasets()
 
 # %%
-# Remove plants not connected to the grid as SE does
-# Data for plants that are not connected to the electrical grid 
-# (These include all plants with a plant_id_eia of 88xxxx and the plants 
-#  in this table: https://github.com/singularity-energy/open-grid-emissions/blob/main/data/manual/plants_not_connected_to_grid.csv)
+# SAMPLE SELECTION
+def eia860_sample_selection(gdf, pdf, udf, odf):
+    # 0. All
+    colname = '0_all'
+    pdf[colname] = True
+    gdf[colname] = True
+    udf[colname] = True
+    odf[colname] = True
+
+    # 1. drop non-contig states
+    colname = '1_drop_noncontig_state'
+    pdf[colname] = ~pdf.state_plant.isin(['AK', 'HI', 'PR'])
+    gdf[colname] = gdf.plant_code.isin(pdf.loc[pdf[colname], 'plant_code'].values)
+    udf[colname] = udf.utility_id.isin(pdf.loc[pdf[colname], 'utility_id'].values)
+    odf[colname] = odf.plant_code.isin(pdf.loc[pdf[colname], 'plant_code'].values)
+
+    # 2. drop CHP plants
+    colname = '2_drop_chp'
+    pdf[colname] = pdf.sector.isin([1,2,4,6]) | pdf.sector.isna()
+    gdf[colname] = gdf.plant_code.isin(pdf.loc[pdf[colname], 'plant_code'].values)
+    udf[colname] = udf.utility_id.isin(pdf.loc[pdf[colname], 'utility_id'].values)
+    odf[colname] = odf.plant_code.isin(pdf.loc[pdf[colname], 'plant_code'].values)
+
+    # SUMMARIZE
+    steps = ['0_all', '1_drop_noncontig_state', '2_drop_chp']
+    step_cols = []
+    summ = pd.DataFrame()
+    for step in steps:
+        step_cols += [step]
+        for idcol, df in zip(['plant_code', 'gid', 'utility_id', 'oid'], [pdf, gdf, udf, odf]):
+            condition = np.array([df[step].values for step in step_cols]).all(0)
+            summdf = (df.loc[condition].groupby('year')
+                        .agg({idcol:'nunique'})
+                        .rename(columns={idcol:(idcol, step)}))
+
+            summ = pd.concat([summdf, summ], axis=1)
+
+    summ = summ[summ.columns.sort_values(ascending=True)]
+    summ.columns = pd.MultiIndex.from_tuples(summ.columns)
+
+    # subset and return
+    gdf_sub = gdf.loc[np.array([gdf[step].values for step in steps]).all(0)]
+    pdf_sub = pdf.loc[np.array([pdf[step].values for step in steps]).all(0)]
+    udf_sub = udf.loc[np.array([udf[step].values for step in steps]).all(0)]
+    odf_sub = odf.loc[np.array([odf[step].values for step in steps]).all(0)]
+    
+    return (gdf_sub, pdf_sub, udf_sub, odf_sub), summ
+
+(gdf_sub, pdf_sub, udf_sub, odf_sub), summ = eia860_sample_selection(gdf, pdf, udf, odf)
+summ
+
+# %%
+# GENERATOR RETIREMENT DATE
+# 1. Take retirement date from latest available year
+gdf_sub['dt_operation_end_from_latest_year'] = np.where(gdf_sub.dt_operation_end.notna(), gdf_sub.year, np.nan)
+gdf_sub['dt_operation_end_from_latest_year'] = gdf_sub.groupby('gid')['dt_operation_end_from_latest_year'].transform('max')
+gdf_sub['dt_operation_end_from_latest_year'] = np.where(gdf_sub.dt_operation_end_from_latest_year == gdf_sub.year, gdf_sub.dt_operation_end, pd.NaT)
+gdf_sub['dt_operation_end_from_latest_year'] = pd.to_datetime(gdf_sub.groupby('gid')['dt_operation_end_from_latest_year'].transform('max'))
+
+# %%
+# Does non-retirement status appear after retirement status?
+gdf_sub.loc[(gdf_sub.status == 'RE') & 
+            (gdf_sub.dt_operation_end_from_latest_year.dt.year > gdf_sub.year)]
+# Yes! Look at case study:
+# gdf_sub.loc[gdf_sub.gid == '1553_3']
+# supported by Wiki: https://en.wikipedia.org/wiki/Gould_Street_Generating_Station
+
+
+# %%
+# TODO: Align EIA entity properties (ones that persist over time)
