47c97ba44c2c9fa888b1f435ace46a79c49da4de
Erich Trieschman <erich.j.trieschman@gmail.com>
2024-01-02 20:52:18 -0500

    CDP flags

--- analysis/cdp_population.py
+++ analysis/cdp_population.py
@@ -1,6 +1,8 @@
 # %%
+import numpy as np
 import pandas as pd
 from pathlib import Path
+from tqdm import tqdm
 
 from utils import format_sumpct_col, format_npct_col, format_mstd_col, format_iqr_col
 from utils import sample_summ
@@ -45,16 +47,22 @@ def preprocess_names(x:pd.Series) -> pd.Series:
     x_pp = (x
         .str.lower()
         .str.replace('\.|\,', '', regex=True)
+        .str.replace('-', ' ', regex=True)
         .str.replace('\s\/[a-zA-Z]{2}\/?', '', regex=True)
-        .str.replace('\\(the\\)', '', regex=True)
-        .str.replace(' inc$| corp$| company$| corporation$| corp$| co$', '', regex=True)
-        .str.replace(' grp$| industries$| international$', '', regex=True)
-        .str.replace(' inc$| corp$| company$| corporation$| corp$| co$', '', regex=True)
-        .str.replace(' grp$| industries$| international$', '', regex=True)
-        .str.replace(' llc| ltd', '', regex=True)
+        .str.replace('\\(.*?\\)', '', regex=True)
         .str.strip()
+        .str.replace(r'\s\b(corp|company|corporation|co)\b(\s|$)', ' co ', regex=True).str.strip()
+        .str.replace(r'\s\b(elec|electric)\b(\s|$)', ' electric ', regex=True).str.strip()
+        .str.replace(r'\s\b(inc|incorporated)\b(\s|$)', ' inc ', regex=True).str.strip()
+        .str.replace(r'\s\b(grp|group)\b(\s|$)', ' group ', regex=True).str.strip()
+        .str.replace(r'\s\b(int|international)\b(\s|$)', ' int ', regex=True).str.strip()
+        .str.replace(r'\s\b(ltd|limited)\b(\s|$)', ' ltd ', regex=True).str.strip()
     )
-    return x_pp
+
+    # one-offs
+    x_pp_oo = (x_pp
+               .str.replace('pacific gas & electric', 'pg&e', regex=True))
+    return x_pp_oo
 
 # cdp
 names_cdp = cdp[['gvkey', 'conm']].drop_duplicates().reset_index(drop=True)
@@ -62,69 +70,98 @@ names_cdp['name_cdp'] = preprocess_names(names_cdp.conm)
 
 # subsidiary (includes year)
 names_cs = csids[['gvkey', 'year', 'coname', 'clean_company']].drop_duplicates().reset_index(drop=True)
-names_cs['name_cs_parent'] = preprocess_names(names_cs.coname)
-names_cs['name_cs_subsid'] = preprocess_names(names_cs.clean_company)
+names_cs['pp_cdp_parname'] = preprocess_names(names_cs.coname)
+names_cs['pp_cdp_subname'] = preprocess_names(names_cs.clean_company)
 
 # EIA datasets (includes year)
-uids = udf[['utility_id', 'utility_name', 'year']].drop_duplicates().reset_index(drop=True)
-uids['name_utility'] = preprocess_names(uids.utility_name)
-oids = odf[['utility_id', 'plant_code', 'generator_id', 'ownership_id', 'owner_name', 'year']].drop_duplicates().reset_index(drop=True)
-oids['name_owner'] = preprocess_names(oids.owner_name)
-pids = pdf[['utility_id', 'plant_code', 'plant_name', 'year']].drop_duplicates().reset_index(drop=True)
-pids['name_plant'] = preprocess_names(pids.plant_name)
+udf['pp_utility_name'] = preprocess_names(udf.utility_name)
+uids = udf[['utility_id', 'utility_name', 'pp_utility_name', 'year']].drop_duplicates().reset_index(drop=True)
+odf['pp_owner_name'] = preprocess_names(odf.owner_name)
+oids = odf[['utility_id', 'plant_code', 'generator_id', 'ownership_id', 'owner_name', 'pp_owner_name', 'year']].drop_duplicates().reset_index(drop=True)
 
 # %%
 # FLAG EIA IDENTIFIERS (ID-YEAR) WITH CDP COMPANIES
-from thefuzz import fuzz
-from thefuzz import process
-import jellyfish
+from thefuzz import fuzz, process
+import concurrent
+import time
 
-df_target = uids
-target_id = 'name_utility'
-df_search = names_cs.copy()
-search_id = 'name_cs_parent'
-num_matches = 5
+def fuzzy_search(df_target, df_search, key_target, key_search, 
+                 scorer, num_matches, threshold):
+    # Step 0: fuzzy search
+    searches = df_search[key_search].drop_duplicates()
+    searches = searches.tolist()
+    targets = df_target[key_target].drop_duplicates()
+    target_map = targets.apply(lambda x: process.extract(x, searches, scorer=scorer, limit=num_matches))
+
+    # Step 1: Expand the list of tuples into separate columns
+    df_expanded = pd.DataFrame(target_map.tolist(), index=targets)
+    df_expanded.columns = [f'pair_{i}' for i in range(num_matches)]
+    
+    # Step 2: Transform to Long Form
+    df_long = df_expanded.melt(var_name='pair', value_name='name_score', ignore_index=False)
+    
+    # Step 3: Split the Tuples into Two Columns
+    df_long[[key_search, f'{key_search}_score']] = pd.DataFrame(df_long['name_score'].tolist(), index=df_long.index)
+    
+    # Drop columns and rows
+    df_long = df_long.drop(['name_score', 'pair'], axis=1)
+    df_long_sub = df_long.loc[df_long[f'{key_search}_score'] >= threshold].reset_index()
+
+    return df_long_sub
+
+def fuzzy_search_years(df_target, df_search, key_target, key_search, 
+                       scorer=fuzz.WRatio, num_matches=1, threshold=95):
+    dfs = pd.DataFrame()
+    for y in tqdm(df_target.year.unique()):
+        df = fuzzy_search(df_target.loc[df_target.year == y],
+                        df_search.loc[df_search.year == y],
+                        key_target, key_search, scorer, num_matches, threshold)
+        df['year'] = y
+        dfs = pd.concat([df, dfs], ignore_index=True)
+    return dfs
+
+# %%
+# CREATE MAPPINGS
+scorer = fuzz.WRatio
+num_matches = 1
 threshold = 95
 
-# fuzzy search
-searches = df_search[search_id].drop_duplicates()
-search = searches.tolist()
-targets = df_target[target_id].drop_duplicates()
-target_map = targets.apply(lambda x: process.extract(x, search, scorer=fuzz.WRatio, limit=num_matches))
+updf = fuzzy_search_years(uids, names_cs, 'pp_utility_name', 'pp_cdp_parname',
+                        scorer, num_matches, threshold)
+usdf = fuzzy_search_years(uids, names_cs, 'pp_utility_name', 'pp_cdp_subname',
+                        scorer, num_matches, threshold)
 
 # %%
-# Step 1: Expand the list of tuples into separate columns
-df_expanded = pd.DataFrame(target_map.tolist(), index=targets)
-df_expanded.columns = [f'pair_{i}' for i in range(num_matches)]
-# Step 2: Transform to Long Form
-df_long = df_expanded.melt(var_name='pair', value_name='name_score', ignore_index=False)
-# Step 3: Split the Tuples into Two Columns
-df_long[[search_id, 'lscore']] = pd.DataFrame(df_long['name_score'].tolist(), index=df_long.index)
-# Drop the original tuple column
-df_long = df_long.drop('name_score', axis=1).reset_index()
-# drop pairs where score is low
-df_long_sub = df_long.loc[df_long.lscore >= threshold]
-df_long_sub.sort_values(by=['name_cs_parent', 'lscore'], ascending=False)
+uspdf = pd.merge(
+    left=usdf, how='left',
+    right=names_cs[['year', 'pp_cdp_parname', 'pp_cdp_subname', 'gvkey']].drop_duplicates(),
+    on=['year', 'pp_cdp_subname'])
 
-
-
-
-
+# %%
+opdf = fuzzy_search_years(oids, names_cs, 'pp_utility_name', 'pp_cdp_parname',
+                        scorer, num_matches, threshold)
+osdf = fuzzy_search_years(oids, names_cs, 'pp_utility_name', 'pp_cdp_subname',
+                        scorer, num_matches, threshold)
 
 
 # %%
-# get operated IDs
-uid_op = udf.loc[udf.utility_name.str.lower().str.contains('|'.join(conms)),
-                 ['utility_id', 'utility_name']].drop_duplicates().reset_index(drop=True)
-uid_op = set(uid_op.utility_id)
+# MERGE CDP FLAGS ONTO UTILITIES AND GENERATORS
+# merge on cdp parent name
+udf_p = pd.merge(left=udf, right=updf, how='outer', 
+                 on=['year', 'pp_utility_name'])
+assert len(udf) == len(udf_p)
+# merge on cdp subsidiary name (this will create dups b/c of partial ownership)
+# NOTE: because subsidiaries are reported for 50% ownership, we expect dups of only magnitute 2
+# i.e., can't have more than 2 companies that own 50%
+udf_ps = pd.merge(left=udf_p, right=uspdf, how='outer',
+                  on=['year', 'pp_utility_name'],
+                  suffixes=['', '_s'])
 
-# get owned IDs
-uid_own = odf.loc[odf.owner_name.str.lower().str.contains('|'.join(conms)),
-                 ['utility_id', 'ownership_id', 'owner_name']].drop_duplicates().reset_index(drop=True)
-uid_own = set(uid_own.utility_id)
 
-# get any IDs
-uid = uid_op.union(uid_own)
+
+
+
+
 
 # %%
 # MAKE ANALYSIS DATASET
